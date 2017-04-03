fa9c3c3dc401a211b0fc20980ae2033998c31853
kmildi <keleti.ildi@gmail.com>
2017-04-03 16:44:34 +0200

    Basic Notes page

---
+++ main.js
@@ -0,0 +1,25 @@
+
+Notes = new Meteor.Collection('notes');
+
+if(Meteor.isClient){
+  Template.savedNotes.helpers({
+	"showNotes":function(){
+	  return Notes.find();
+  	}
+  });
+  Template.writeInForm.events({
+	"submit form": function(evt){
+		evt.preventDefault();
+		var typedTextVar = evt.target.typedText.value;
+		Notes.insert({
+			text: typedTextVar,
+		});
+		evt.target.typedText.value = "";
+    	}
+  });
+
+}
+ 
+if(Meteor.isServer){
+   
+}
